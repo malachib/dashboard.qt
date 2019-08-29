0d9c41a0ab55e8fe029e9fb844d3dee7d73dd905
Malachi Burke <malachi.burke@gmail.com>
2019-08-28 20:44:00 -0700

    Reformatting file according to PyCharm preferences

--- src/qt-ui/main.py
+++ src/qt-ui/main.py
@@ -15,15 +15,15 @@ from pyqt.Geocoder import Geocoder
 
 import pyqt.HDMI
 
-#geocode_test1()
+# geocode_test1()
 
 init_subsystem()
 
-#raw_test3()
+# raw_test3()
 
 app = QApplication(sys.argv)
 engine = QQmlEngine(app)
-#engine = QQmlApplicationEngine()
+# engine = QQmlApplicationEngine()
 
 # doesn't quite work because we aren't actually using the component associated
 # with this engine
@@ -45,14 +45,14 @@ view = QQuickView()
 
 engine2 = view.engine
 # Does not run
-#engine2.quit.connect(app.quit)
+# engine2.quit.connect(app.quit)
 
-#view.setSource(QUrl('PyTest.qml'))
+# view.setSource(QUrl('PyTest.qml'))
 # To Satisfy cool-retro-term needs
 view.rootContext().setContextProperty('devicePixelRatio', app.devicePixelRatio())
 
 view.setSource(QUrl('WeatherDash.qml'))
-#view.setResizeMode(QDeclarativeView.SizeRootObjectToView)
+# view.setResizeMode(QDeclarativeView.SizeRootObjectToView)
 view.setGeometry(100, 100, 750, 480)
 # ala https://pythonspot.com/pyqt5-colors/
 view.setColor(QColor(0, 30, 0))
@@ -61,12 +61,14 @@ view.show()
 
 is_full_screen = False
 
+
 # technique lifted from https://stackoverflow.com/questions/19131084/pyqt5-qml-signal-to-python-slot
 # and augmented from https://stackoverflow.com/questions/30586983/how-to-close-pyqt5-program-from-qml
 # could refine with https://stackoverflow.com/questions/24111717/how-to-bind-buttons-in-qt-quick-to-python-pyqt-5
 # not 100% ideal, but adequate and interesting
 def on_quit():
     app.quit()
+
 
 def on_toggle_fullscreen():
     global is_full_screen
@@ -76,15 +78,16 @@ def on_toggle_fullscreen():
         view.showFullScreen()
     is_full_screen = not is_full_screen
 
+
 weather_dash = view.rootObject()
 weather_dash.quit.connect(on_quit)
 weather_dash.toggleFullScreen.connect(on_toggle_fullscreen)
 
 # may want to do this slightly differently, though I prefer QML doing the heavy lifting
 # http://doc.qt.io/qt-5/qqmlcomponent.html
-#weather_chart = view.findChild(Loader, "WeatherChart")
+# weather_chart = view.findChild(Loader, "WeatherChart")
 
 # this does work
-#view.showFullScreen()
+# view.showFullScreen()
 
 app.exec_()
